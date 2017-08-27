b80b9714f07f2c7da699ccfc7db074057cb4f306
hpneo <hpneo@hotmail.com>
2017-08-27 11:40:00 -0500

    Skip verify_authenticity_token

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -1,4 +1,6 @@
 class CertificatesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def check
     certificate = Certificate.where(code: params[:code]).first
 
