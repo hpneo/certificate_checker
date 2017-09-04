0ec802cfd5a17463eb31edc2eeea52f93d5afcd1
hpneo <hpneo@hotmail.com>
2017-09-04 06:55:39 -0500

    Redirect root to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   post '/certificates/check' => 'certificates#check'
+
+  root to: redirect('/admin')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
